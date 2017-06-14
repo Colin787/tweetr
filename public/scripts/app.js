999ca8dd728eefb7076cf944c23820892a8d4cd2
colin787 <colinpark4@gmail.com>
2017-06-14 16:49:17 +0000

    added 2nd feature to dynamic tweets (for loop and an array)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Test / driver code (temporary). Eventually will get this from the server.
-var tweetData = {
+/*var tweetData = {
   "user": {
     "name": "Newton",
     "avatars": {
@@ -18,40 +18,91 @@ var tweetData = {
     "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
-}
+}*/
+
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 $(document).ready(function(){
-  function createTweetElement(newTweetData){
-
-    var tweetContent = newTweetData.content.text;
-    var tweetHandle = newTweetData.user.handle;
-    var tweetUsername = newTweetData.user.name;
-    var tweetAvatar = newTweetData.user.avatars.regular;
-    var tweetCreatedAt = newTweetData.created_at;
-  return `
-    <article class="single-tweet">
-
-      <header class="tweets-header">
-
-        <span class="userAt">${tweetHandle}</span>
-        <img ${tweetAvatar} class="myimg"><h1 class="nameField">${tweetUsername}</h1>
-      </header>
-
-      <div class="tweet-content">
-       <p>${tweetContent}</p>
-      </div>
-
-      <footer class="tweetFooter">
-        <span class="timeSent">${tweetCreatedAt}<span class="icons">⚑ ⇆ ❤</span></span>
-      </footer>
-    </article>`;
-
-
-}
-var $tweet = createTweetElement(tweetData);
+  function createTweetElement(data){
+    var tweetContent = data.content.text;
+    var tweetHandle = data.user.handle;
+    var tweetUsername = data.user.name;
+    var tweetAvatar = data.user.avatars.regular;
+    var tweetCreatedAt = data.created_at;
+    return `
+      <article class="single-tweet">
+        <header class="tweets-header">
+          <span class="userAt">${tweetHandle}</span>
+          <img ${tweetAvatar} class="myimg"><h1 class="nameField">${tweetUsername}</h1>
+        </header>
+        <div class="tweet-content">
+         <p>${tweetContent}</p>
+        </div>
+        <footer class="tweetFooter">
+          <span class="timeSent">${tweetCreatedAt}<span class="icons">⚑ ⇆ ❤</span></span>
+        </footer>
+      </article>`;
+  }
+  function renderTweets(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for(var i = 0; i < tweets.length; i++) {
+      var $tweet = createTweetElement(tweets[i]);
+      $('.allTweets').append($tweet);
+    }
+  }
+  renderTweets(data);
+});
+/*var $tweet = createTweetElement(tweetData);
 console.log($tweet); // to see what it looks like
 $('.allTweets').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 // Test / driver code (temporary)
-
+*/
 
 
