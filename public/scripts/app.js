e7385b2b0e9b290805e80bd6c9d2b4393dd08a36
colin787 <colinpark4@gmail.com>
2017-06-14 23:05:29 +0000

    included ajax and refactored code with instructor

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,65 +3,24 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
 $(document).ready(function(){
+  function escape(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
   function createTweetElement(data){
-    var tweetContent = data.content.text;
-    var tweetHandle = data.user.handle;
-    var tweetUsername = data.user.name;
+    var tweetContent = escape(data.content.text);
+    var tweetHandle = escape(data.user.handle);
+    var tweetUsername = escape(data.user.name);
     var tweetAvatar = data.user.avatars.regular;
-    var tweetCreatedAt = data.created_at;
+    var tweetCreatedAt = moment(data.created_at).fromNow();;
     return `
       <article class="single-tweet">
         <header class="tweets-header">
           <span class="userAt">${tweetHandle}</span>
-          <img ${tweetAvatar} class="myimg"><h1 class="nameField">${tweetUsername}</h1>
+          <img src="${tweetAvatar}" class="myimg"><h1 class="nameField">${tweetUsername}</h1>
         </header>
         <div class="tweet-content">
          <p>${tweetContent}</p>
@@ -75,12 +34,37 @@ $(document).ready(function(){
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
+    $('.allTweets').empty();
     for(var i = 0; i < tweets.length; i++) {
       var $tweet = createTweetElement(tweets[i]);
-      $('.allTweets').append($tweet);
+      $('.allTweets').prepend($tweet);
     }
   }
-  renderTweets(data);
+
+  loadTweets();
+
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets/',
+    }).done(renderTweets)
+  }
+
+  $(".tweet-form").on("submit", function(event) {
+    event.preventDefault();
+    if($(".textboxArea").val().length > 140 || $(".textboxArea").val() === "") {
+      alert("I QUIT");
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets/',
+        data: $(this).serialize()
+      }).done(function() {
+        loadTweets();
+      });
+    }
+  });
+
 });
 
 
