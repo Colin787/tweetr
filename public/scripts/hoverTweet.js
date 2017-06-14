328d7a95be0df07272e285db025ef4bb0ee550db
colin787 <colinpark4@gmail.com>
2017-06-14 16:09:49 +0000

    not using this anymore...CSS hover is simple

--- public/scripts/hoverTweet.js
+++ public/scripts/hoverTweet.js
@@ -1,7 +1,26 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $(".single-tweet").hover(function() {
     $(".tweet-content").after($("<div class=icons><span class=flag>⚑</span> <span class=retweet>⇆</span> <span class=heart>❤</span></div>"));
   }, function() {
     $(this).find("div:last").remove();
   });
-});
+});*/
+/*$(document).ready(function() {
+  function createFAIcon(faID) {
+    var faElement = $('<i>');
+    faElement.addClass('fa ' + faID);
+    faElement.attr('aria-hidden', 'true');
+    return faElement;
+  }
+  $(".single-tweet").hover(function() {
+      var iconContainer = $('<div>');
+      iconContainer.addClass('icons');
+      var flagIcon = createFAIcon('fa-flag');
+      var retweetIcon = createFAIcon('fa-retweet');
+      var heartIcon = createFAIcon('fa-heart');
+      iconContainer.append(flagIcon).append(retweetIcon).append(heartIcon);
+      $('.tweet-content').append(iconContainer);
+    }, function() {
+      $(this).find('.tweet-content').find("div:last").remove();
+    });
+});*/
