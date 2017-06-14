e89312ad9556b22056fc7d6a1833e2653e74bb85
colin787 <colinpark4@gmail.com>
2017-06-14 00:42:31 +0000

    count characters function in jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,12 @@
 $(document).ready(function() {
  $('textarea').on('keyup', function() {
-   let $counter = $('.new-tweet .counter');
-   let $character = $(this).val().length;
-   if ($character < 140) {
-      $counter.removeClass('over').text(`Characters remaining ${140 - $character}`);
-    }
-   if ($character > 140) {
-      $counter.addClass('over').text(`TOO MANY CHARACTERS ${140 - $character}`);
-      $counter.css('color', 'red');
+   let $counter = $(this).siblings(".counter");
+   let numCharacters = $(this).val().length;
 
-    } else {
-      $counter.css('color', 'black');
-    }
+   if (numCharacters > 140) {
+     $counter.addClass('over').text(`TOO MANY CHARACTERS ${140 - numCharacters}`);
+   } else {
+     $counter.removeClass('over').text(`Characters remaining ${140 - numCharacters}`);
+   }
   });
 });
